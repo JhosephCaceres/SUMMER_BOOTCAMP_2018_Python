364f1b0bd05416725539e0dd399e96db5784bb94
root <root@localhost.localdomain>
2018-02-05 16:03:39 -0400

    JCA L1T1 HOLA MUNDO

--- lesson1/task1/hello_world.py
+++ lesson1/task1/hello_world.py
@@ -2,6 +2,5 @@
 Tradicionalmente ó, el primer programa que escribe en cualquier lenguaje de programacion es "Hello World!".
 Presentese al mundo.
 Ejecutar el script
-jose
 """
 print("Welcome to Bootcamp 2018")
