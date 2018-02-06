2683d2a5fa366ed1de7db45b5718523dd1cbd401
root <root@localhost.localdomain>
2018-02-06 11:14:17 -0400

    JCA L2T1

--- lesson2/task1/variable_definition.py
+++ lesson2/task1/variable_definition.py
@@ -1,8 +1,12 @@
-a=b =2
-print("a="+str(a))
-print("b ="+str(b))
+"""
+Los comentarios en Python comienzan con el carácter (#) e incluyen toda la línea.
+Agregue un nuevo comentario al archivo.
+"""
+a = b = 2  # Esto se llama una "asignación encadenada". Asigna el valor 2 a las variables "a" y "b".
+print("a = " + str(a))   # Explicaremos la expresión str(a) más adelante en el curso. Por ahora, se usa para convertir la variable "a" en una cadena.
+print("b = " + str(b))
 
-saludos= "saludos"
-print("saludos="+ str(saludos))
-saludos =3
-print("saludos="+str(saludos))
+saludos = "saludos" # greetings
+print("saludos = " + saludos)
+saludos = 3
+print("saludos = " + str(saludos))
