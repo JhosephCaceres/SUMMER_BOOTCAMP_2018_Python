0f01201e09cb317ff808533becbd4e9e13d164d4
root <root@localhost.localdomain>
2018-02-06 11:15:54 -0400

    JCA L2T2

--- lesson2/task2/undefined_variable.py
+++ lesson2/task2/undefined_variable.py
@@ -1,2 +1,9 @@
-clase = 1
-print("clase")
+"""
+Los nombres de las variables solo pueden contener letras,
+dígitos  o el carácter de subrayado, y no pueden comenzar con un dígito.
+
+Compruebe lo que sucede si utiliza una variable que aún no está definida.
+Intenta imprimir un nombre indefinido.
+"""
+variable = 1
+print(variable)
